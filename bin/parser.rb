97550d1ba99dd902297a3d94c6e90c0d38ad9090
S1rFrancis <francisj2011@gmail.com>
2017-03-28 21:18:34 +0100

    move parser.rb script to bin directory

---
+++ bin/parser.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/logs/log'
+require_relative '../lib/logs/page_views'
+
+logs = []
+File.readlines(ARGV[0]).each do |line|
+  values = line.split(" ")
+  logs << Log.new(values[0], values[1])
+end
+
+page_views = PageViews.new(logs)
+
+puts "View count for each page: "
+page_views.views.each { |page, count| puts page + " " + count.to_s + " visits" }
+puts
+puts "Unique view count for each page: "
+page_views.uniqueViews.each { |page, count| puts page + " " + count.to_s + " visits" }
